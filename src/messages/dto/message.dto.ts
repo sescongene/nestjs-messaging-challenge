361df8bf60b23e4deeb2b8bc827a75383e18c4ac
Gene Sescon <sescongene@gmail.com>
2022-04-04 14:44:53 +0800

    added sanitizer

--- src/messages/dto/message.dto.ts
+++ src/messages/dto/message.dto.ts
@@ -1,8 +1,11 @@
 import { IsNotEmpty } from 'class-validator';
+import { Trim } from 'class-sanitizer';
 
 export class MessageDto {
   @IsNotEmpty()
   conversationId: string;
+
+  @Trim()
   @IsNotEmpty()
   message: string;
 }
